dd6b8cb6034ad3b42e283652a024038b948e0403
Zhuo Chen <chen519@usc.edu>
2018-12-15 15:45:34 -0800

    first submit of LogR

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Logistic-Regression
+	Implement Logistic Regression for binary and multiclass classification.
+		1
+			In this problem, you are given atraining set D = {(xn, yn)N}, where yi ∈ {0, 1} ∀i = 1...N. 
+		Important: note that here the binary labels are not −1 or +1, so be very careful about applying formulas.
+		The task is to learn the linear model specified by wTx + b that minimizes the logistic loss. Note that we
+		do not explicitly append the feature 1 to the data, so you need to explicitly learn the bias/intercept term
+		b too. Specifically you need to implement function binary_train in logistic.py which uses gradient
+		descent (not stochastic gradient descent) to find the optimal parameters (recall logistic regression does not
+		admit a closed-form solution).
+			In addition you need to implement function binary_predict in logistic.py. There are two ways of making 
+		predictions in logistic regression: deterministic prediction or randomized prediction. Here you need to use 
+		the deterministic prediction.
+			After finishing implementation, please run logistic_binary.sh which generates logistic_binary.out.
+
+		2
+			There are several methods to perform multiclass classification. One of them was one-versus-rest or 
+		one-versus-all approach.
+			For one-versus-rest classification in a problem with K classes, we need to train K classifiers using a
+		black-box. Classifier k is trained on a binary problem, where the two labels corresponds to belonging or
+		not belonging to class k. After that, the multiclass prediction is made based on the combination of all
+		predictions from K binary classifiers.
+			In this problem you will implement one-versus-rest using binary logistic regression (that you have implemented
+		in part 1) as the black-box. Important: the way used to predict is to randomized over the classifiers that say “yes”;
+		however, here since binary logistic regression naturally predicts a probability for each class (recall the sigmoid model), 
+		we will simply predict the class with the highest probability (using numpy argmax).
+			To sum up, you need to complete functions OVR_train and OVR_predict to perform one-versus-rest
+		classification. After you finished implementation, please run logistic_multiclass.sh script, which
+		will produce logistic_multiclass.out.
+
+		3
+			Yet another multiclass classification method was multinomial logistic regression. Complete the functions 
+		multinomial_train and multinomial_predict to perform multinomial logistic regression, following the same notes 
+		as in part 1, that is, 
+			1) explicitly learn the biased term; 
+			2) perform gradient descent instead of stochastic gradient descent; 
+			3) make deterministic predictions.
+			After you finished implementation, please run logistic_multiclass.sh script, which will produce
+		logistic_multiclass.out.
+
+		Advice 
+			We are extensively using softmax and sigmoid function. To avoid numerical issues such as overflow and underflow 
+		caused by numpy.exp() and numpy.log(), please use the following implementations:
+			• Let x be a input vector to the softmax function. Use x~ = x − max(x) instead of using x directly for the
+			  softmax function f. That is, if you want to compute f(x)i, compute f(x~)i = exp(x~i) / sum(exp(x~i)) instead, 
+			  which is clearly mathematically equivalent but numerically more stable.
+			• If you are using numpy.log(), make sure the input to the log function is positive. Also, there may
+			  be chances that one of the outputs of softmax, e.g. f(x~)i, is extremely small but you need the value
+			  ln(f(x~)i). In this case you should convert the computation equivalently into x~i − ln(sum(exp(x~i)))
---
+++ logistic.py
@@ -0,0 +1,395 @@
+from __future__ import division, print_function
+
+import numpy as np
+from scipy import stats
+
+
+#######################################################################
+# Replace TODO with your code
+#######################################################################
+
+def binary_train(X, y, w0=None, b0=None, step_size=0.5, max_iterations=1000):
+    """
+    Inputs:
+    - X: training features, a N-by-D numpy array, where N is the 
+    number of training points and D is the dimensionality of features
+    - y: binary training labels, a N dimensional numpy array where 
+    N is the number of training points, indicating the labels of 
+    training data
+    - step_size: step size (learning rate)
+	- max_iterations: number of iterations to perform gradient descent
+
+    Returns:
+    - w: D-dimensional vector, a numpy array which is the weight 
+    vector of logistic regression
+    - b: scalar, which is the bias of logistic regression
+
+    Find the optimal parameters w and b for inputs X and y.
+    Use the *average* of the gradients for all training examples
+	multiplied by the step_size to update parameters.
+    """
+    N, D = X.shape
+    assert len(np.unique(y)) == 2
+
+
+    w = np.zeros(D)
+    if w0 is not None:
+        w = w0
+    
+    b = 0
+    if b0 is not None:
+        b = b0
+
+
+    """
+    TODO: add your code here
+    """
+    # y label is 0 or 1 not 1 or -1
+    y = np.array(y).reshape(N, 1)
+    iterations = 1
+    while iterations < max_iterations:
+        iterations += 1
+        a = -sigmoid(np.dot(X, w.reshape(D,1)) + b) + y
+        wMean = np.mean(a * X, axis = 0)
+        bMean = np.mean(a)
+        '''
+        #another method
+        y = 2 * y - 1
+        wG = sigmoid(-np.dot(X, w.reshape(D,1)) * y) * y * X
+        bG = sigmoid(-b * y) * y
+        wMean = np.mean(wG, axis = 0)
+        bMean = np.mean(bG)
+        '''
+        b += step_size * bMean
+        w += step_size * wMean
+    assert w.shape == (D,)
+    return w, b
+
+
+def binary_predict(X, w, b):
+    """
+    Inputs:
+    - X: testing features, a N-by-D numpy array, where N is the 
+    number of training points and D is the dimensionality of features
+    
+    Returns:
+    - preds: N dimensional vector of binary predictions: {0, 1}
+    """
+    N, D = X.shape
+    preds = np.zeros(N) 
+
+
+    """
+    TODO: add your code here
+    """  
+    preds = np.round(sigmoid(np.dot(X, w.reshape(D, 1)) + b)).reshape(N,)
+    assert preds.shape == (N,) 
+    return preds
+
+
+def multinomial_train(X, y, C, 
+                     w0=None, 
+                     b0=None, 
+                     step_size=0.5, 
+                     max_iterations=1000):
+    """
+    Inputs:
+    - X: training features, a N-by-D numpy array, where N is the 
+    number of training points and D is the dimensionality of features
+    - y: multiclass training labels, a N dimensional numpy array where
+    N is the number of training points, indicating the labels of 
+    training data
+    - C: number of classes in the data
+    - step_size: step size (learning rate)
+    - max_iterations: number of iterations to perform gradient descent
+
+    Returns:
+    - w: C-by-D weight matrix of multinomial logistic regression, where 
+    C is the number of classes and D is the dimensionality of features.
+    - b: bias vector of length C, where C is the number of classes
+
+    Implement multinomial logistic regression for multiclass 
+    classification. Again use the *average* of the gradients for all training 
+	examples multiplied by the step_size to update parameters.
+	
+	You may find it useful to use a special (one-hot) representation of the labels, 
+	where each label y_i is represented as a row of zeros with a single 1 in
+    the column, that corresponds to the class y_i.
+    """
+
+    N, D = X.shape
+
+    w = np.zeros((C, D))
+    if w0 is not None:
+        w = w0
+    
+    b = np.zeros(C)
+    if b0 is not None:
+        b = b0
+
+
+    """
+    TODO: add your code here
+    """
+    
+    iterations = 1
+    while iterations < max_iterations:
+        iterations += 1
+        b_ = np.tile(b, (N,1))
+        a = np.dot(X, np.transpose(w)) + b_
+        aMax = np.max(a, axis = 1).reshape(N,1)
+        a_ = a - np.tile(aMax, (1,C))
+        a_exp = np.exp(a_)
+        a_expSum = np.sum(a_exp, axis = 1).reshape(N,1)
+        p = a_exp / np.tile(a_expSum, (1,C))
+        wGSum = np.zeros((C, D))
+        bGSum = np.zeros(C)
+        for i in range(N):
+            p[i][y[i]] -= 1
+        bMean = np.sum(p, axis=0) / N
+        wMean = np.dot(np.transpose(p), X) / N
+        w -= step_size * wMean
+        b -= step_size * bMean
+
+    assert w.shape == (C, D)
+    assert b.shape == (C,)
+    return w, b
+
+
+def multinomial_predict(X, w, b):
+    """
+    Inputs:
+    - X: testing features, a N-by-D numpy array, where N is the 
+    number of training points and D is the dimensionality of features
+    - w: weights of the trained multinomial classifier
+    - b: bias terms of the trained multinomial classifier
+    
+    Returns:
+    - preds: N dimensional vector of multiclass predictions.
+    Outputted predictions should be from {0, C - 1}, where
+    C is the number of classes
+
+    Make predictions for multinomial classifier.
+    """
+    N, D = X.shape
+    C = w.shape[0]
+    preds = np.zeros(N) 
+
+    """
+    TODO: add your code here
+    """   
+
+    b_ = np.tile(b, (N,1))
+    a = np.dot(X, np.transpose(w)) + b_
+    preds = np.argsort(a)[:,C - 1]
+
+    assert preds.shape == (N,)
+    return preds
+
+
+def OVR_train(X, y, C, w0=None, b0=None, step_size=0.5, max_iterations=1000):
+    """
+    Inputs:
+    - X: training features, a N-by-D numpy array, where N is the 
+    number of training points and D is the dimensionality of features
+    - y: multiclass training labels, a N dimensional numpy array, 
+    indicating the labels of each training point
+    - C: number of classes in the data
+    - w0: initial value of weight matrix
+    - b0: initial value of bias term
+    - step_size: step size (learning rate)
+    - max_iterations: number of iterations to perform gradient descent
+
+    Returns:
+    - w: a C-by-D weight matrix of OVR logistic regression
+    - b: bias vector of length C
+
+    Implement multiclass classification using one-versus-rest with binary logistic 
+	regression as the black-box. Recall that the one-versus-rest classifier is 
+    trained by training C different classifiers. 
+    """
+    N, D = X.shape
+    
+    w = np.zeros((C, D))
+    if w0 is not None:
+        w = w0
+    
+    b = np.zeros(C)
+    if b0 is not None:
+        b = b0
+
+    """
+    TODO: add your code here
+    """
+
+    for i in range(C):
+        # if class i then y = 1; if not class i then y = 0
+        yNew = np.round(np.exp(-np.abs(y - i)))
+        w[i], b[i] = binary_train(X, yNew, w[i], b[i], step_size, max_iterations)
+
+    assert w.shape == (C, D), 'wrong shape of weights matrix'
+    assert b.shape == (C,), 'wrong shape of bias terms vector'
+    return w, b
+
+
+def OVR_predict(X, w, b):
+    """
+    Inputs:
+    - X: testing features, a N-by-D numpy array, where N is the 
+    number of training points and D is the dimensionality of features
+    - w: weights of the trained OVR model
+    - b: bias terms of the trained OVR model
+    
+    Returns:
+    - preds: vector of class label predictions.
+    Outputted predictions should be from {0, C - 1}, where
+    C is the number of classes.
+
+    Make predictions using OVR strategy and probability predictions from binary
+    classifiers. 
+    """
+    N, D = X.shape
+    C = w.shape[0]
+
+    """
+    TODO: add your code here
+    """
+    b_ = np.tile(b, (N,1))
+    a = sigmoid(np.dot(X, np.transpose(w)) + b_)
+    # the max probability column is the (C - 1) th
+    preds = np.argsort(a)[:,C - 1]
+
+    assert preds.shape == (N,)
+    return preds
+
+
+#######################################################################
+# DO NOT MODIFY THE CODE BELOW 
+#######################################################################
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def accuracy_score(true, preds):
+    return np.sum(true == preds).astype(float) / len(true)
+
+def run_binary():
+    from data_loader import toy_data_binary, \
+                            data_loader_mnist 
+
+    print('Performing binary classification on synthetic data')
+    X_train, X_test, y_train, y_test = toy_data_binary()
+        
+    w, b = binary_train(X_train, y_train)
+    
+    train_preds = binary_predict(X_train, w, b)
+    preds = binary_predict(X_test, w, b)
+    print('train acc: %f, test acc: %f' % 
+            (accuracy_score(y_train, train_preds),
+             accuracy_score(y_test, preds)))
+    
+    print('Performing binary classification on binarized MNIST')
+    X_train, X_test, y_train, y_test = data_loader_mnist()
+
+    binarized_y_train = [0 if yi < 5 else 1 for yi in y_train] 
+    binarized_y_test = [0 if yi < 5 else 1 for yi in y_test] 
+    
+    w, b = binary_train(X_train, binarized_y_train)
+    
+    train_preds = binary_predict(X_train, w, b)
+    preds = binary_predict(X_test, w, b)
+    print('train acc: %f, test acc: %f' % 
+            (accuracy_score(binarized_y_train, train_preds),
+             accuracy_score(binarized_y_test, preds)))
+
+def run_multiclass():
+    from data_loader import toy_data_multiclass_3_classes_non_separable, \
+                            toy_data_multiclass_5_classes, \
+                            data_loader_mnist 
+    
+    datasets = [(toy_data_multiclass_3_classes_non_separable(), 
+                        'Synthetic data', 3), 
+                (toy_data_multiclass_5_classes(), 'Synthetic data', 5), 
+                (data_loader_mnist(), 'MNIST', 10)]
+
+    for data, name, num_classes in datasets:
+        print('%s: %d class classification' % (name, num_classes))
+        X_train, X_test, y_train, y_test = data
+        
+        print('One-versus-rest:')
+        w, b = OVR_train(X_train, y_train, C=num_classes)
+        train_preds = OVR_predict(X_train, w=w, b=b)
+        preds = OVR_predict(X_test, w=w, b=b)
+        print('train acc: %f, test acc: %f' % 
+            (accuracy_score(y_train, train_preds),
+             accuracy_score(y_test, preds)))
+    
+        print('Multinomial:')
+        w, b = multinomial_train(X_train, y_train, C=num_classes)
+        train_preds = multinomial_predict(X_train, w=w, b=b)
+        preds = multinomial_predict(X_test, w=w, b=b)
+        print('train acc: %f, test acc: %f' % 
+            (accuracy_score(y_train, train_preds),
+             accuracy_score(y_test, preds)))
+
+
+if __name__ == '__main__':
+    
+    import argparse
+    import sys
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--type", )
+    parser.add_argument("--output")
+    args = parser.parse_args()
+
+    if args.output:
+            sys.stdout = open(args.output, 'w')
+
+    if not args.type or args.type == 'binary':
+        run_binary()
+
+    if not args.type or args.type == 'multiclass':
+        run_multiclass()
+
+'''
+    a = np.array([1,2,3,4,5])
+    b = np.array([3,7,8,4,6])
+    c = np.array([2,1,5,0,4])
+    d = a + b
+    e = np.array([[1,2,3,4,5],[3,7,8,4,6],[2,1,5,0,4]])
+    f_ = np.array([1,2,3,2,1])
+    f = np.array(np.matrix(f_).T)
+    g = np.dot(e,f)
+    h = ([1,0,1])
+   
+    h_ = np.matrix(h).T
+    h_ = 2 * h_ -1 ;
+    j = g * np.array(h_)
+    k = sigmoid(j)
+    l = k * np.array(h_);
+    m = l * e
+    wMean = np.mean(m, axis = 0)
+    wFinal = f_ + wMean
+    
+    print(h_)
+    print(j)
+    print(k)
+    print(l)
+    print(m)
+    print(wFinal)
+
+    print(e.shape)
+    print(f.shape)
+    print(g.shape)
+      
+    print(e)
+    print(f)
+    print(g + 12)
+
+    w = np.zeros(7)
+    wM = np.matrix(w)
+    w_ = np.transpose(wM)
+    print(w_.shape)
+    '''
